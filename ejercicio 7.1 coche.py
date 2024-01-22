d7d83d711158eb39b77255927d204402e119df1a
Jacintotatu <jacintoconesa@hotmail.com>
2024-01-22 23:45:30 +0100

    ok

---
+++ ejercicio 7.1 coche.py
@@ -0,0 +1,2 @@
+coche = input('¿Que tipo de coche desea alquilar?: ')
+print(f"Veamos si tenemos un {coche} para usted.")
---
+++ ejercicio 7.2 restaurant.py
@@ -0,0 +1,5 @@
+comensales = int(input("¿Cuantos vendran a cenar?: "))
+if comensales > 8:
+    print("Ok, tendran que esperar una mesa.")
+else:
+    print("Adelante, su mesa esta lista.")
