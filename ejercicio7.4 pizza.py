3d45c294ff1c86bc8fc793fa53bcf3877ad0a73e
Jacintotatu <jacintoconesa@hotmail.com>
2024-01-25 23:25:43 +0100

    ok

--- ejercicio7.4 pizza.py
+++ ejercicio7.4 pizza.py
@@ -1,37 +1,17 @@
-"""
-pizza = "\nIngresa un ingrediente para la pizza: "
-pizza_no = "\nEscribe 'quit' para quitar el programa."
-
-mensaje = ""
-
-active = True
-while active:
-    mensaje = input(pizza)
-
-    if mensaje == 'quit':
-        active = False
-        
-    else:
-        print(f"Ok, añadiremos el ingrediente {mensaje} a la pizza.")
- """ 
 
 pizza = "\nIngresa un ingrediente para la pizza: "
 pizza_no = "\nEscribe 'quit' para quitar el programa."
 
-#print(f"{pizza}")
+
 print(f"{pizza_no}")
-
 mensaje = ""
-ingredientes = []
 while mensaje != 'quit':
     mensaje = input(pizza)
-    print(f"Ok, añadiremos el ingrediente {mensaje} a la pizza.")
-    ingredientes.append(mensaje)
-
+    
     if mensaje == 'quit':
         print('Adios')
-        ingredientes.pop()
+    else:
+        print(f"\nOk, añadiremos el ingrediente {mensaje} a la pizza.")
 
-        break
+    
 
-print(ingredientes)
---
+++ ejercicio7.6con,br,act.py
@@ -0,0 +1,52 @@
+
+""" 
+****************BREAK**********************
+pizza = "\nIngresa un ingrediente para la pizza: "
+pizza_no = "\nEscribe 'quit' para quitar el programa."
+
+print(f"{pizza_no}")
+
+mensaje = ""
+while mensaje != 'quit':
+    mensaje = input(pizza)
+    
+    if mensaje == 'quit':
+        print('Adios')
+        break
+    else:
+        print(f"Ok, añadiremos el ingrediente {mensaje} a la pizza.")
+
+*****************VARIABLE ACTIVE******************
+pizza = "\nIngresa un ingrediente para la pizza: "
+pizza_no = "\nEscribe 'quit' para quitar el programa."
+
+
+print(f"{pizza_no}")
+mensaje = ""
+active = True
+while mensaje != 'quit':
+    mensaje = input(pizza)
+    
+    if mensaje == 'quit':
+        print('Adios')
+        active = False
+    else:
+        print(f"\nOk, añadiremos el ingrediente {mensaje} a la pizza.")
+"""
+******************condicional**********************
+pizza = "\nIngresa un ingrediente para la pizza: "
+pizza_no = "\nEscribe 'quit' para quitar el programa."
+
+
+print(f"{pizza_no}")
+mensaje = ""
+while mensaje != 'quit':
+    mensaje = input(pizza)
+    
+    if mensaje == 'quit':
+        print('Adios')
+    else:
+        print(f"\nOk, añadiremos el ingrediente {mensaje} a la pizza.")
+
+    
+
