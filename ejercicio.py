b3a6a517ba4d284562e05723664305a606258836
Jacintotatu <jacintoconesa@hotmail.com>
2024-01-30 00:25:18 +0100

    se imprime el quit como ingrediente

--- ejercicio.py
+++ ejercicio.py
@@ -1,3 +1,39 @@
+"""
+ingredientes = []
 
+print('Cuando no quieras mas pon "quit".')
+
+while True:
+    ingrediente = input('Introduce un ingrediente para tu pizza: ')
+    if ingrediente == "quit":
+        print('Adeu')
+        break
+    
+    print(f'Ok, añadiremos {ingrediente.title()} a tu pizza.')
+    ingredientes.append(ingrediente)
+
+print('Los ingredientes de la pizza son: ') 
+for i in ingredientes:
+    
+    print(f'                         *{i.title()}')
+"""
+
+ingredientes = []
+
+print('Cuando no quieras mas pon "quit".')
+bandera = True
+while bandera:
+    ingrediente = input('Introduce un ingrediente para tu pizza: ')
+    if ingrediente == "quit":
+        print('Adeu')
+        bandera = False
+    
+    print(f'Ok, añadiremos {ingrediente.title()} a tu pizza.')
+    ingredientes.append(ingrediente)
+
+print('Los ingredientes de la pizza son: ') 
+for i in ingredientes:
+    
+    print(f'                         *{i.title()}')
 
 
